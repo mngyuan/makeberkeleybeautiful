f9c2ad334ab4760f862fd78da1b50ca97e4f2ed9
Julia Sun <julia.sun@berkeley.edu>
2016-04-13 00:00:20 -0700

    started doing photo tier

---
+++ src/blog.js
@@ -0,0 +1,15 @@
+const stickyHeader = _ => {
+  const win = $(window);
+  const postHeader = $('.post-header');
+
+  if (win.scrollTop() <= $('.post-content').offset().top - postHeader.outerHeight(true)) {
+    postHeader.removeClass('sticky');
+  } else if (win.scrollTop() >= postHeader.offset().top) {
+    postHeader.addClass('sticky');
+  }
+};
+
+$(document).ready(_ => {
+  $(window).scroll(stickyHeader);
+  stickyHeader();
+});
